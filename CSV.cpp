43aa2d87da45fe456c175b72376d420a8f002f7b
William Zhao <william.zhao@stud.unifi.it>
2023-03-29 17:09:35 +0200

    0-50 images test for 3 filter

--- CSV.cpp
+++ CSV.cpp
@@ -13,6 +13,23 @@ CSV::CSV(std::string f) : filename(f) {
 
 }
 
+void CSV::customWrite(const std::string h, std::vector<int> t, const int size){
+    std::fstream fout;
+
+    // opens an existing csv file or creates a new file.
+    fout.open(filename, std::ios::out);
+
+    fout << h << "\n";
+
+    const int sizeVector = t.size();
+    for(int i = 0; i < sizeVector; i++){
+        fout << std::to_string(i+1) + "," + std::to_string(t[i]) + "\n";
+    }
+
+    fout.close();
+
+}
+
 void CSV::write(const std::vector<int> &sTimes,
                 const std::vector<int> &pTimes,
                 const std::vector<float> &spTimes,
@@ -26,8 +43,8 @@ void CSV::write(const std::vector<int> &sTimes,
 
     for(int i = 0; i < size; i++){
         std::string formattedText = std::to_string(i+1) + "," +
-                std::to_string(sTimes.at(i)) + "," + std::to_string(pTimes.at(i)) + "," +
-                std::to_string(spTimes.at(i));
+                                    std::to_string(sTimes.at(i)) + "," + std::to_string(pTimes.at(i)) + "," +
+                                    std::to_string(spTimes.at(i));
         fout << formattedText << "\n";
     }
 
--- main.cu
+++ main.cu
@@ -6,6 +6,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include "png.h"
+#include "CSV.h"
 #include "KernelImageProcessing.h"
 
 namespace fs = std::filesystem;
@@ -22,6 +23,8 @@ namespace fs = std::filesystem;
         exit(1);                                                \
                                                                 \
     }}
+
+std::vector<int> times;
 
 char GaussianBlurkernel[FILTER_SIZE * FILTER_SIZE] = {
         1, 2, 1,
@@ -103,11 +106,11 @@ int main()
     std::string path = "../images/";
     std::string targetPath = "../filtered_images/";
     for (const auto & entry : fs::directory_iterator(path)){
-        //images.push_back(entry.path());
+        images.push_back(entry.path());
         //std::cout << entry.path() << std::endl;
     }
     //images.push_back("../images/road-7504719-4500.png");
-    images.push_back("../images/road-7504719-6000.png");
+    //images.push_back("../images/road-7504719-6000.png");
     //images.push_back("../images/road-7504719_1920.png");
     //images.push_back("../images/road-7504719_1280.png");
     //images.push_back("../images/road-7504719_640.png");
@@ -136,171 +139,184 @@ int main()
 
 
 
-    for(int i = 0; i < images.size();i++){
-        std::string filePath = images[i];
+    for(int k = 0; k < 50; k++) {
+        bool init = false;
+        std::chrono::steady_clock::time_point begin;
+        for (int i = 0; i < k; i++) {
+            std::string filePath = images[i];
 
-        std::cout << "Loading image..." << std::endl;
+            std::cout << "Loading image..." << std::endl;
 
-        png::image<png::rgb_pixel> input_image(filePath);
-        unsigned int width = input_image.get_width();
-        unsigned int height = input_image.get_height();
+            png::image<png::rgb_pixel> input_image(filePath);
+            unsigned int width = input_image.get_width();
+            unsigned int height = input_image.get_height();
 
-        std::cout << "Width: " << width << " Height:" << height << std::endl;
+            std::cout << "Width: " << width << " Height:" << height << std::endl;
 
-        unsigned int size = width * height * sizeof(unsigned char);
+            unsigned int size = width * height * sizeof(unsigned char);
 
-        //Red, green, blue pixels from input image
-        unsigned char* input_image_pixel_red = (unsigned char*)malloc(size);
-        unsigned char* input_image_pixel_green = (unsigned char*)malloc(size);
-        unsigned char* input_image_pixel_blue = (unsigned char*)malloc(size);
+            //Red, green, blue pixels from input image
+            unsigned char *input_image_pixel_red = (unsigned char *) malloc(size);
+            unsigned char *input_image_pixel_green = (unsigned char *) malloc(size);
+            unsigned char *input_image_pixel_blue = (unsigned char *) malloc(size);
 
-        //Output from kernel function (convolution)
-        unsigned char *output_image_pixel_red = (unsigned char *) malloc(size);
-        unsigned char *output_image_pixel_green = (unsigned char *) malloc(size);
-        unsigned char *output_image_pixel_blue = (unsigned char *) malloc(size);
+            //Output from kernel function (convolution)
+            unsigned char *output_image_pixel_red = (unsigned char *) malloc(size);
+            unsigned char *output_image_pixel_green = (unsigned char *) malloc(size);
+            unsigned char *output_image_pixel_blue = (unsigned char *) malloc(size);
 
-        //Store pixel in variables
-        pngio::pngToRgb3(input_image_pixel_red, input_image_pixel_green,input_image_pixel_blue,input_image);
+            //Store pixel in variables
+            pngio::pngToRgb3(input_image_pixel_red, input_image_pixel_green, input_image_pixel_blue, input_image);
 
-
-        // Allocate memory on GPU
-        unsigned char* input_data_gpu_red;
-        unsigned char* input_data_gpu_green;
-        unsigned char* input_data_gpu_blue;
-
-        size_t pitch_r = 0;
-        size_t pitch_g = 0;
-        size_t pitch_b = 0;
-
-
-        cudaMallocPitch(&input_data_gpu_red,&pitch_r,width,height);
-        cudaMallocPitch(&input_data_gpu_green,&pitch_g,width,height);
-        cudaMallocPitch(&input_data_gpu_blue,&pitch_b,width,height);
-
-        cudaMemcpy2D(input_data_gpu_red,pitch_r,input_image_pixel_red,width,width,height,cudaMemcpyHostToDevice);
-        cudaMemcpy2D(input_data_gpu_green,pitch_g,input_image_pixel_green,width,width,height,cudaMemcpyHostToDevice);
-        cudaMemcpy2D(input_data_gpu_blue,pitch_b,input_image_pixel_blue,width,width,height,cudaMemcpyHostToDevice);
-
-
-        dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
-        dim3 gridSize((width + TILE_SIZE -1)/TILE_SIZE,(height + TILE_SIZE - 1)/TILE_SIZE);
-
-        unsigned char* output_data_gpu_red;
-        unsigned char* output_data_gpu_green;
-        unsigned char* output_data_gpu_blue;
-
-        cudaMalloc(&output_data_gpu_red,size);
-        cudaMalloc(&output_data_gpu_green,size);
-        cudaMalloc(&output_data_gpu_blue,size);
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-
-        for(int j = 0; j < filters.size();j++) {
-
-            char* kernel = NULL;
-            cudaMalloc(&kernel,FILTER_SIZE*FILTER_SIZE);
-            cudaMemcpy(kernel,filters[j],FILTER_SIZE*FILTER_SIZE,cudaMemcpyHostToDevice);
-
-            const int factorKernel = factor[j];
-
-
-
-            convolution<<<gridSize, blockSize>>>(output_data_gpu_red, input_data_gpu_red, pitch_r, kernel, factorKernel, width, height);
-            convolution<<<gridSize, blockSize>>>(output_data_gpu_green, input_data_gpu_green, pitch_g,kernel, factorKernel, width, height);
-            convolution<<<gridSize, blockSize>>>(output_data_gpu_blue, input_data_gpu_blue, pitch_b,kernel, factorKernel, width, height);
-
-            cudaDeviceSynchronize();
-
-
-
-            cudaMemcpy(output_image_pixel_red, output_data_gpu_red, size, cudaMemcpyDeviceToHost);
-            cudaMemcpy(output_image_pixel_green, output_data_gpu_green, size, cudaMemcpyDeviceToHost);
-            cudaMemcpy(output_image_pixel_blue, output_data_gpu_blue, size, cudaMemcpyDeviceToHost);
-
-            // Convert output data to PNG image
-            //pngio::rgb3toPng(output_image_pixel_red,output_image_pixel_green,output_image_pixel_blue,input_image);
-
-            //std::string targetFilePathName = targetPath + filtersName[j] + "_" +  std::string(fs::path(filePath).filename());
-            //input_image.write(targetFilePathName);
-            //std::cout << "Saved in " << targetFilePathName << std::endl;
-
-            cudaFree(kernel);
-        }
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        int timeDifference = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
-        std::cout << "Elapsed Time: " << timeDifference << std::endl;
-
-
-
-
-
-        cudaFree(output_data_gpu_red);
-        cudaFree(output_data_gpu_green);
-        cudaFree(output_data_gpu_blue);
-        cudaFree(input_data_gpu_red);
-        cudaFree(input_data_gpu_green);
-        cudaFree(input_data_gpu_blue);
-
-        free(output_image_pixel_red);
-        free(output_image_pixel_green);
-        free(output_image_pixel_blue);
-        free(input_image_pixel_red);
-        free(input_image_pixel_blue);
-        free(input_image_pixel_green);
-
-
-        /*
-        for(int j = 0; j < filters.size();j ++){
-
-            cudaMemcpy(kernel_gpu, filters[j], KERNEL_SIZE * KERNEL_SIZE * sizeof(float), cudaMemcpyHostToDevice);
-            convolution_3channel<<<grid_size, block_size>>>(input_data_gpu, output_data_gpu, width, height, 3, kernel_gpu, KERNEL_SIZE);
-            cudaMemcpy(input_data, output_data_gpu, width * height * 3 * sizeof(float), cudaMemcpyDeviceToHost);
-
-            // Convert output data to PNG image
-            idx = 0;
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    int red = (int)(input_data[idx++] );
-                    int green = (int)(input_data[idx++] );
-                    int blue = (int)(input_data[idx++] );
-                    output_image[y][x] = png::rgb_pixel(red, green, blue);
-                }
+            if(init == false){
+                init = true;
+                begin = std::chrono::steady_clock::now();
             }
 
-            std::string targetFilePathName = targetPath + filtersName[j] + "_" + std::string(fs::path( filePath ).filename());
-            output_image.write(targetFilePathName);
-            std::cout << "Saved in " << targetFilePathName << std::endl;
+            // Allocate memory on GPU
+            unsigned char *input_data_gpu_red;
+            unsigned char *input_data_gpu_green;
+            unsigned char *input_data_gpu_blue;
+
+            size_t pitch_r = 0;
+            size_t pitch_g = 0;
+            size_t pitch_b = 0;
+
+
+            cudaMallocPitch(&input_data_gpu_red, &pitch_r, width, height);
+            cudaMallocPitch(&input_data_gpu_green, &pitch_g, width, height);
+            cudaMallocPitch(&input_data_gpu_blue, &pitch_b, width, height);
+
+            cudaMemcpy2D(input_data_gpu_red, pitch_r, input_image_pixel_red, width, width, height,
+                         cudaMemcpyHostToDevice);
+            cudaMemcpy2D(input_data_gpu_green, pitch_g, input_image_pixel_green, width, width, height,
+                         cudaMemcpyHostToDevice);
+            cudaMemcpy2D(input_data_gpu_blue, pitch_b, input_image_pixel_blue, width, width, height,
+                         cudaMemcpyHostToDevice);
+
+
+            dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
+            dim3 gridSize((width + TILE_SIZE - 1) / TILE_SIZE, (height + TILE_SIZE - 1) / TILE_SIZE);
+
+            unsigned char *output_data_gpu_red;
+            unsigned char *output_data_gpu_green;
+            unsigned char *output_data_gpu_blue;
+
+            cudaMalloc(&output_data_gpu_red, size);
+            cudaMalloc(&output_data_gpu_green, size);
+            cudaMalloc(&output_data_gpu_blue, size);
+
+
+            for (int j = 0; j < 1; j++) {
+
+                char *kernel = NULL;
+                cudaMalloc(&kernel, FILTER_SIZE * FILTER_SIZE);
+                cudaMemcpy(kernel, filters[j], FILTER_SIZE * FILTER_SIZE, cudaMemcpyHostToDevice);
+
+                const int factorKernel = factor[j];
+
+
+                convolution<<<gridSize, blockSize>>>(output_data_gpu_red, input_data_gpu_red, pitch_r, kernel,
+                                                     factorKernel, width, height);
+                convolution<<<gridSize, blockSize>>>(output_data_gpu_green, input_data_gpu_green, pitch_g, kernel,
+                                                     factorKernel, width, height);
+                convolution<<<gridSize, blockSize>>>(output_data_gpu_blue, input_data_gpu_blue, pitch_b, kernel,
+                                                     factorKernel, width, height);
+
+                cudaDeviceSynchronize();
+
+
+                cudaMemcpy(output_image_pixel_red, output_data_gpu_red, size, cudaMemcpyDeviceToHost);
+                cudaMemcpy(output_image_pixel_green, output_data_gpu_green, size, cudaMemcpyDeviceToHost);
+                cudaMemcpy(output_image_pixel_blue, output_data_gpu_blue, size, cudaMemcpyDeviceToHost);
+
+                // Convert output data to PNG image
+                //pngio::rgb3toPng(output_image_pixel_red,output_image_pixel_green,output_image_pixel_blue,input_image);
+
+                //std::string targetFilePathName = targetPath + filtersName[j] + "_" +  std::string(fs::path(filePath).filename());
+                //input_image.write(targetFilePathName);
+                //std::cout << "Saved in " << targetFilePathName << std::endl;
+
+                cudaFree(kernel);
+            }
+
+
+
+            cudaFree(output_data_gpu_red);
+            cudaFree(output_data_gpu_green);
+            cudaFree(output_data_gpu_blue);
+            cudaFree(input_data_gpu_red);
+            cudaFree(input_data_gpu_green);
+            cudaFree(input_data_gpu_blue);
+
+            free(output_image_pixel_red);
+            free(output_image_pixel_green);
+            free(output_image_pixel_blue);
+            free(input_image_pixel_red);
+            free(input_image_pixel_blue);
+            free(input_image_pixel_green);
+
+
+            /*
+            for(int j = 0; j < filters.size();j ++){
+
+                cudaMemcpy(kernel_gpu, filters[j], KERNEL_SIZE * KERNEL_SIZE * sizeof(float), cudaMemcpyHostToDevice);
+                convolution_3channel<<<grid_size, block_size>>>(input_data_gpu, output_data_gpu, width, height, 3, kernel_gpu, KERNEL_SIZE);
+                cudaMemcpy(input_data, output_data_gpu, width * height * 3 * sizeof(float), cudaMemcpyDeviceToHost);
+
+                // Convert output data to PNG image
+                idx = 0;
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int red = (int)(input_data[idx++] );
+                        int green = (int)(input_data[idx++] );
+                        int blue = (int)(input_data[idx++] );
+                        output_image[y][x] = png::rgb_pixel(red, green, blue);
+                    }
+                }
+
+                std::string targetFilePathName = targetPath + filtersName[j] + "_" + std::string(fs::path( filePath ).filename());
+                output_image.write(targetFilePathName);
+                std::cout << "Saved in " << targetFilePathName << std::endl;
+            }
+
+
+
+
+
+
+
+
+
+
+            cudaFree(input_data_gpu);
+            cudaFree(output_data_gpu);
+            cudaFree(kernel_gpu);
+            /*
+            for(int j = 0; j < filters.size();j ++){
+                std::cout << "Convoluting with "+filtersName[j]+" kernel matrix... " << std::endl;
+                Image filteredImage = kip.convolute(image,filters[j],true);
+                std::cout << "Convolution completed!" << std::endl;
+
+                std::string targetFilePathName = targetPath + filtersName[j] + "_" + std::string(fs::path( filePath ).filename());
+                kip.saveImage(filteredImage, targetFilePathName.c_str());
+                filteredImage.clear();
+
+            }
+
+             */
+
+
         }
-
-
-
-
-
-
-
-
-
-
-        cudaFree(input_data_gpu);
-        cudaFree(output_data_gpu);
-        cudaFree(kernel_gpu);
-        /*
-        for(int j = 0; j < filters.size();j ++){
-            std::cout << "Convoluting with "+filtersName[j]+" kernel matrix... " << std::endl;
-            Image filteredImage = kip.convolute(image,filters[j],true);
-            std::cout << "Convolution completed!" << std::endl;
-
-            std::string targetFilePathName = targetPath + filtersName[j] + "_" + std::string(fs::path( filePath ).filename());
-            kip.saveImage(filteredImage, targetFilePathName.c_str());
-            filteredImage.clear();
-
-        }
-
-         */
-
-
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        int timeDifference = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+        std::cout << "Elapsed Time: " << timeDifference << std::endl;
+        times.push_back(timeDifference);
     }
 
-
+    CSV v("../data.csv");
+    std::cout << "test Time size: " << times.size() << std::endl;
+    v.customWrite("Length,seq_time",times,times.size());
 
     return 0;
 }
